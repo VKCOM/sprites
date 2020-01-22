3767081b4f7eb260302cd03ff257def1facb84d8
Egor Smirnov <esmirnov.xyz@gmail.com>
2020-01-22 11:33:29 +0300

    Unit test (#12)
    * unit tests
    
    * add example docs
    
    * screenshot unit tests
    
    * split example verify to function
    
    * test script

---
+++ __tests__/lib.test.js
@@ -0,0 +1,113 @@
+const tmp = require('tmp');
+const fs = require('fs-extra');
+const puppeteer = require('puppeteer');
+const { promisify } = require('util');
+const path = require('path');
+const async = require("async");
+
+const generate = require('../src/index');
+const InkscapeConverter = require("../src/convert/InkscapeConverter");
+
+const { toMatchImageSnapshot } = require('jest-image-snapshot');
+expect.extend({ toMatchImageSnapshot });
+
+const createTempDir = promisify(tmp.dir);
+tmp.setGracefulCleanup();
+
+const baseSpriteOptions = {
+  css: {
+    stylesheetPrefix: "sprite-"
+  },
+  svg: {
+    dest: "/images/mobile/icons/svg/"
+  },
+  png: {
+    scalePrefix: "vk_",
+    class: "png",
+    dest: "/images/mobile/icons/svg"
+  }
+};
+
+async function getTempOutputPaths() {
+  const tempDir = await createTempDir();
+
+  const pngPath = path.resolve(tempDir, "png");
+  const svgPath = path.resolve(tempDir, "svg");
+  const cssPath = path.resolve(tempDir, "css");
+  const examplePath = path.resolve(tempDir, "example");
+
+  return {
+    pngPath,
+    svgPath,
+    cssPath,
+    examplePath
+  };
+}
+
+async function verifyExamples(examplePath) {
+  const browser = await puppeteer.launch();
+  const exampleFiles = await fs.readdir(examplePath);
+
+  const examplesNameMap = exampleFiles
+    .map(name => name.split('-'))
+    .reduce((acc, splittedName) => {
+      acc[splittedName.join('-')] = splittedName.slice(0, -1).join('-')
+
+      return acc;
+    }, {});
+
+
+  await async.forEachOf(exampleFiles, async file => {
+    {
+      const page = await browser.newPage();
+
+      await page.goto('file://' + path.resolve(examplePath, file));
+      const image = await page.screenshot();
+
+      expect(image).toMatchImageSnapshot({
+        customSnapshotIdentifier: examplesNameMap[file],
+      });
+    }
+  });
+
+  await browser.close();
+}
+
+it('works without example', async () => {
+  const input = path.resolve(__dirname, "./fixtures/render");
+
+  const { pngPath, svgPath, cssPath } = await getTempOutputPaths();
+
+  await generate(
+    input,
+    {
+      pngPath,
+      svgPath,
+      cssPath,
+    },
+    null,
+    baseSpriteOptions
+  );
+});
+
+describe('renders correctly', () => {
+  it('without PNG converter', (async () => {
+    const input = path.resolve(__dirname, "./fixtures/render");
+
+    const { pngPath, svgPath, cssPath, examplePath } = await getTempOutputPaths();
+
+    await generate(
+      input,
+      {
+        pngPath,
+        svgPath,
+        cssPath,
+        examplePath
+      },
+      null,
+      baseSpriteOptions
+    );
+    
+    await verifyExamples(examplePath);
+  }));
+});
--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ const lstat = promisify(fs.lstat);
  * @param {string} output.pngPath Path to folder where PNG sprites will be saved
  * @param {string} output.svgPath Path to folder where SVG sprites will be saved
  * @param {string} output.cssPath Path to folder where stylesheets will be saved
+ * @param {string} output.examplePath Path to folder where stylesheets will be saved
  * @param {BaseConverter} converter SVG to PNG converter
  * @param {Object} options
  * @param {Object} options.css
@@ -31,9 +32,10 @@ const lstat = promisify(fs.lstat);
  * @param {string} options.png.dest Path to SVG where it should be placed on website
  * @param {string} options.png.class PNG class in top of DOM (e.g in the `<head>` or `<body>`) to detect fallback need
  * @param {string} options.png.scalePrefix Prefix for class with needed scale. E.g `png_2x` or `fallback_1x`
+ * @param {Object} options.example
  */
 async function generate(path, output = {}, converter, options) {
-  const { pngPath, svgPath, cssPath } = output;
+  const { pngPath, svgPath, cssPath, examplePath } = output;
 
   const defaultOptions = {
     css: {
@@ -53,14 +55,24 @@ async function generate(path, output = {}, converter, options) {
   options = mergeDeep(defaultOptions, options);
 
   await checkDir(path);
-  await checkDir(pngPath);
   await checkDir(svgPath);
   await checkDir(cssPath);
+  
+  if (pngPath) {
+    await checkDir(pngPath);
+  }
+  if (examplePath) {
+    await checkDir(examplePath);
+  }
 
   const gc = new GarbageCollector();
 
   gc.addPath(pngPath);
   gc.addPath(svgPath);
+  
+  if (examplePath) {
+    gc.addPath(examplePath);
+  }
 
   const baseConfigGenerator = () => ({
     shape: {
@@ -98,6 +110,11 @@ async function generate(path, output = {}, converter, options) {
         common: options.css.class,
         dimensions: true,
         bust: true,
+        example: examplePath ? {
+          example: {
+            dest: examplePath
+          }
+        } : false,
         render: {
           css: true
         }
@@ -131,7 +148,7 @@ async function generate(path, output = {}, converter, options) {
         },
         variables: {
           options,
-          scales: converter.scales
+          scales: converter ? converter.scales: []
         }
       };
 
@@ -148,12 +165,20 @@ async function generate(path, output = {}, converter, options) {
         spriter.compile(async (err, res) => {
           const css = join(cssPath, res.css.less.basename);
           const svg = join(svgPath, res.css.sprite.basename);
+          let example = examplePath && join(examplePath, res.css.example.basename);
 
           const hash = gc.extractHash(svg);
           gc.addHash(hash);
 
           await writeFile(css, res.css.less.contents.toString());
           await writeFile(svg, res.css.sprite.contents.toString());
+
+          if (example) {
+            example = example
+              .replace('sprite.css.html', basename(svg, '.svg') + '.html');
+
+            await writeFile(example, res.css.sprite.contents.toString());
+          }
 
           if (converter) {
             await convert(
